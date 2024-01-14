89a4d61e901eb5a351c2782df7e0e15817752978
sector127 <gio.shatira@gmail.com>
2024-01-14 23:03:51 +0400

    webhook-5

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -65,13 +65,9 @@ exports.getChckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.display_items[0].amount_total / 100;
-  await Booking.create({
-    tour,
-    user,
-    price,
-  });
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total / 100;
+  await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
